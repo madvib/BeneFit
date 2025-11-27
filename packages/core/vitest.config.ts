a5daf18a8ddda2b6a8028932afd912349caa2e71
madvib <micahcotton17@gmail.com>
2025-11-27 21:00:30 +0000

    Update vitest config in core for simpler path compatibility

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
-
-import { fileURLToPath } from 'url'; // Required for ES Module compatibility
-
-// Define a helper for __dirname in ES module context
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+
   test: {
     setupFiles: ['./test/setup.ts'],
     globals: true,
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname,
+    },
   },
 });
